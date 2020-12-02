895ac5eb321ad1d3539ed5fdc4882f51dbc50463
juaancmendez <juancho_3494@hotmail.com>
2020-12-02 00:14:14 -0300

    cambios

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row pt-5">
             <div class="col-3 col-sm-2 mt-2 text-center text-sm-left">
-                <a href="https://github.com" target="_blank" title="Github"><i class="fab fa-github fa-2x mr-1"></i></a>
+                <a href="https://github.com/juaancmendez" target="_blank" title="Github"><i class="fab fa-github fa-2x mr-1"></i></a>
                 <a href="https://linkedin.com/in/nelson-daniel-tarche" target="_blank" title="Linkedin"><i class="fab fa-linkedin fa-2x"></i></a>
             </div>
             <div class="col-3 col-sm-2 mt-2">
