8f3569f362b4a48f8be3e863a5b8ae849b46462c
juaancmendez <juancho_3494@hotmail.com>
2020-12-01 23:59:11 -0300

    cambios

--- footer.php
+++ footer.php
@@ -1,26 +1,22 @@
 <footer>
-        <div class="container">
-            <div class="row pt-5">
-                <div class="col-3 col-sm-2 mt-2 text-center text-sm-left">
-                    <a href="https://github.com" target="_blank" title="Github"><i
-                            class="fab fa-github fa-2x mr-1"></i></a>
-                    <a href="https://linkedin.com/in/nelson-daniel-tarche" target="_blank" title="Linkedin"><i
-                            class="fab fa-linkedin fa-2x"></i></a>
-                </div>
-                <div class="col-3 col-sm-2 mt-2">
-                    Sponsor <a href="https://depcsuite.com" target="_blank" title="DePC Suite"><span
-                            class="subrayar">DePC Suite</span></a>
-                </div>
-                <div class="col-3 col-sm-2 mt-2 text-right">
-                    <a href="mailto:">info@dominio.com.ar</a>
-                </div>
-                <div class="col-3 col-sm-6 text-right">
-                    <a href="http://web.whatsapp/com" target="_blank" title="Whatsapp"><i id="icono-whatsapp"
-                            class="fab fa-whatsapp fa-3x"></i></a>
-                </div>
+    <div class="container">
+        <div class="row pt-5">
+            <div class="col-3 col-sm-2 mt-2 text-center text-sm-left">
+                <a href="https://github.com" target="_blank" title="Github"><i class="fab fa-github fa-2x mr-1"></i></a>
+                <a href="https://linkedin.com/in/nelson-daniel-tarche" target="_blank" title="Linkedin"><i class="fab fa-linkedin fa-2x"></i></a>
+            </div>
+            <div class="col-3 col-sm-2 mt-2">
+                Sponsor <a href="https://depcsuite.com" target="_blank" title="DePC Suite"><span class="subrayar">DePC Suite</span></a>
+            </div>
+            <div class="col-3 col-sm-2 mt-2 text-right">
+                <a href="mailto:">info@dominio.com.ar</a>
+            </div>
+            <div class="col-3 col-sm-6 text-right">
+                <a href="http://web.whatsapp/com" target="_blank" title="Whatsapp"><i id="icono-whatsapp" class="fab fa-whatsapp fa-3x"></i></a>
             </div>
         </div>
-    </footer>
+    </div>
+</footer>
 </body>
 
 </html>
--- menu.php
+++ menu.php
@@ -13,33 +13,30 @@
     <script src="js/jquery-3.5.1.min.js"></script>
     <script src="css/bootstrap/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="css/estilos.css">
-    <link
-        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </head>
 
-<body id=<?php echo " $pg "?>>
+<body id=<?php echo " $pg " ?>>
     <header>
         <div class="container">
             <nav class="navbar navbar-expand-md px-0">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault"
-                    aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
                     <i class="fa fa-bars"></i>
                 </button>
                 <div class="row collapse navbar-collapse mb-3 mt-3" id="navbarsExampleDefault">
                     <div class="col-4 col-sm-6 px-2">
                         <ul class="navbar-nav mr-auto">
                             <li class="nav-item">
-                                <a class="nav-link <?php echo $pg == "inicio"? "active" : ""; ?>" href="index.php">Home</a>
+                                <a class="nav-link <?php echo $pg == "inicio" ? "active" : ""; ?>" href="index.php">Home</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link <?php echo $pg == "sobre-mi"? "active" : ""; ?>" href="sobre-mi.php">Sobre mi</a>
+                                <a class="nav-link <?php echo $pg == "sobre-mi" ? "active" : ""; ?>" href="sobre-mi.php">Sobre mi</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link <?php echo $pg == "proyectos"? "active" : ""; ?>" href="proyectos.php">Proyectos</a>
+                                <a class="nav-link <?php echo $pg == "proyectos" ? "active" : ""; ?>" href="proyectos.php">Proyectos</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link <?php echo $pg == "contacto"? "active" : ""; ?>" href="contacto.php">Contacto</a>
+                                <a class="nav-link <?php echo $pg == "contacto" ? "active" : ""; ?>" href="contacto.php">Contacto</a>
                             </li>
                         </ul>
                     </div>
